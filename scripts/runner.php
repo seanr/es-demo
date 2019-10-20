aabf69b8ad25d4b223a93f30a8d62228768cfe64
Sean Robertson <seanr@webolutionary.com>
2019-10-19 20:34:41 -0400

    Remove the runner for now, contained in Lando now

--- scripts/runner.php
+++
@@ -1,8 +0,0 @@
-<?php
-set_time_limit(0); // make it run forever
-//$pid=shell_exec('nohup /usr/local/bin/php /oac/search/php-daemon/wcm-daemon/wcm-daemon.php WcmCrawlWorker >/dev/null &');
-//while(true) {
-//  $exe=shell_exec('/usr/local/bin/php /app/search/php-daemon/wcm-scheduler/sqs-scheduler.php >> /data/logs/cron.log');
-//  sleep(60);
-//}
-$exe=shell_exec('/app/norconex/norconex-jef-monitor-4.0.5/jef-monitor.sh >/dev/null 2>&1');
