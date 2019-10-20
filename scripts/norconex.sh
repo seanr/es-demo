2e80606556df1070f7f9b4494dd984d0a9c2c677
Sean Robertson <seanr@webolutionary.com>
2019-10-19 20:03:33 -0400

    Further demo progress

---
+++ scripts/norconex.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf /app/norconex/
+
+# Needs to exist.
+touch /var/log/search/worker.log
+
+# Move to Norconex directory.
+mkdir /app/norconex/
+cd /app/norconex/
+
+# Add Norconex components.
+wget -NS --content-disposition 'https://oss.sonatype.org/content/repositories/releases/com/norconex/collectors/norconex-collector-http/2.8.1/norconex-collector-http-2.8.1.zip'
+wget -NS --content-disposition 'https://oss.sonatype.org/content/repositories/releases/com/norconex/jef/norconex-jef-monitor/4.0.5/norconex-jef-monitor-4.0.5.zip'
+wget -NS --content-disposition 'https://oss.sonatype.org/content/repositories/releases/com/norconex/collectors/norconex-committer-elasticsearch/4.1.0/norconex-committer-elasticsearch-4.1.0.zip'
+
+# Extract all.
+unzip -o norconex-collector-http-2.8.1.zip; rm norconex-collector-http-2.8.1.zip
+unzip -o norconex-jef-monitor-4.0.5.zip; rm norconex-jef-monitor-4.0.5.zip
+unzip -o norconex-committer-elasticsearch-4.1.0.zip; rm norconex-committer-elasticsearch-4.1.0.zip
+
+# Build Norconex.
+chmod a+x /app/norconex/norconex-jef-monitor-4.0.5/jef-monitor.sh
+chmod a+x /app/norconex/norconex-committer-elasticsearch-4.1.0/install.sh
+/app/scripts/norconex.expect
