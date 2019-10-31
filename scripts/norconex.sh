d3a03460d27476d49642c00d509592c5c010a7b4
Sean Robertson <seanr@webolutionary.com>
2019-10-31 17:29:32 -0400

    Port fixes, comment fixes and config reorg

--- scripts/norconex.sh
+++ scripts/norconex.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Blow away existing stack - this will only happen on rebuild.
 rm -rf /app/norconex/
 
-# Needs to exist.
+# Ensure presence of log file.
 touch /var/log/search/worker.log
 
 # Move to Norconex directory.
@@ -19,10 +20,13 @@ unzip -o norconex-committer-elasticsearch-4.1.0.zip; rm norconex-committer-elast
 wget -NS --content-disposition 'https://oss.sonatype.org/content/repositories/releases/com/norconex/jef/norconex-jef-monitor/4.0.5/norconex-jef-monitor-4.0.5.zip'
 unzip -o norconex-jef-monitor-4.0.5.zip; rm norconex-jef-monitor-4.0.5.zip
 
-# Build Norconex.
+# Copy Norconex config.
 cp /app/config/jefconfig.xml /app/norconex/norconex-jef-monitor-4.0.5/config/jefconfig.xml
+cp /app/config/setup.properties /app/norconex/norconex-jef-monitor-4.0.5/config/setup.properties
+
+# Permissions for Norconex monitor and installer.
 chmod a+x /app/norconex/norconex-jef-monitor-4.0.5/jef-monitor.sh
 chmod a+x /app/norconex/norconex-committer-elasticsearch-4.1.0/install.sh
 
-# Required for interactive responses to Norconex ES committer installer.
+# Interactive responses for Norconex ES committer installer.
 /app/scripts/norconex.expect
